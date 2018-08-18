54af03e68176dad4ebfd65f47677eef35b70dda0
alisashaykk <alisaya7@yandex.ru>
2018-08-18 23:43:23 +0300

    first

---
+++ assets/js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+  $(".owl-carousel").owlCarousel({
+	  items:1, 
+	  touchDrag:true,
+	  loop: true,
+	  autoplay: true,
+	  autoplayTimeout: 3000,
+	  /*autoplayHoverPause: true,*/
+	  smartSpeed: 1000
+  });
+});
